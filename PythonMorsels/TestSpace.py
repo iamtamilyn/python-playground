7952b0eb7c8a3e6efbf46df821f1fb7b90b9147f
Tamilyn <tl.tamilyn@gmail.com>
2020-10-09 19:40:13 -0600

    design patterns course learning

--- PythonMorsels/TestSpace.py
+++ PythonMorsels/TestSpace.py
@@ -1,33 +1,12 @@
-NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-         'julian sequeira', 'sandra bullock', 'keanu reeves',
-         'julbob pybites', 'bob belderbos', 'julian sequeira',
-         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+class practice:
+    global_name = "TL"
+
+    def __init__(self, name):
+        self.name = name
 
 
-def dedup_and_title_case_names(names):
-    """Should return a list of title cased names,
-       each name appears only once"""
-    return list(set([x.title() for x in names]))
+tsp = practice("Tamilyn")
 
-def sort_by_surname_desc(names):
-    """Returns names list sorted desc by surname"""
-    names = dedup_and_title_case_names(names)
-    def surname(val):
-        return val.split(" ")[1]
-    names.sort(key=surname)
-    return names
-
-def shortest_first_name(names):
-    """Returns the shortest first name (str).
-       You can assume there is only one shortest name.
-    """
-    names = dedup_and_title_case_names(names)
-    def surname(val):
-        return len(val.split(" ")[0])
-    names.sort(reverse=True, key=surname)
-    return names[0].split(" ")[0]
-
-
-print(dedup_and_title_case_names(NAMES))
-print(sort_by_surname_desc(NAMES))
-print(shortest_first_name(NAMES))
+print(tsp)
+print(tsp.name)
+print(tsp.global_name)
